d6c6f2d3437cd90e350f32adbcbca15e54a7685f
InMemoLab <59032451+inmemolab@users.noreply.github.com>
2022-06-01 20:43:18 -0500

    fix: 🐛 for run in heroku

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,14 @@ import bodyParser from "body-parser";
 import apiRouter from "./routes";
 // montamos app/express
 const app = express();
+// set port
+const port = process.env.PORT || 3000;
 // use bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // routes api and app
 app.use("/api/", apiRouter);
 // app
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("App iniciada");
 });
