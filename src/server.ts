a0cc1b8271ae602a6fb2ffd8535a78055dcb81a0
InMemoLab <59032451+inmemolab@users.noreply.github.com>
2022-06-01 20:51:45 -0500

    fix: 🐛 for ini get heroku

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 // importamos express
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 // routes
 import apiRouter from "./routes";
@@ -10,6 +10,8 @@ const port = process.env.PORT || 3000;
 // use bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// show
+app.get("/", (req: Request, res: Response) => res.send("Listos!!!"));
 // routes api and app
 app.use("/api/", apiRouter);
 // app
